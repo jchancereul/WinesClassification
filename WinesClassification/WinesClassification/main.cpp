ed7168955bc413f04a2179156453f03d702db6ab
jchancereul <julien.chnc@gmail.com>
2014-02-07 00:05:55 +0100

    Predict processed Log of the work remaining

--- WinesClassification/WinesClassification/main.cpp
+++ WinesClassification/WinesClassification/main.cpp
@@ -86,4 +86,75 @@ void main()
 	//Print the accuracy of the model
 	printf("Correct classification: %f%% \n Incorrect classification: %f%%\n",
 		(float)good_prediction / nb_var * 100, (float)bad_prediction / nb_var * 100);
+
+	//We now have a MLP model and we also have the quality of the prediction thanks to the testing part
+	//We can use the model to predict the label of other external data
+
+	//Preliminary step: load the saved model from the HDD
+	CvANN_MLP nnmodel;
+	CvFileStorage* saved_model = cvOpenFileStorage("C:\\Users\\Julien\\Documents\\ENSAE\\C++\\Projet\\WinesClassification\\Results\\trained_models\\trained_model.xml", 0, CV_STORAGE_READ);
+	CvFileNode *node = cvGetFileNodeByName(saved_model, 0, "WinesClassification");
+	nnmodel.read(saved_model, node);
+	cvReleaseFileStorage(&saved_model);
+
+	//1- We have a whole matrix of samples to classify
+
+	int nb_predict_samples = 100;
+
+	//We still have to convert the data into an OpenCV matrix
+	//We don't need the labels because we want to act as if we haven't them but it is much easier to use the same GetData method
+	//so we will define two cv::Mat but use only one
+
+	cv::Mat predict_var;
+	cv::Mat predict_labels;
+	cv::Mat predict_results;
+
+	GetData("C:\\Users\\Julien\\Documents\\ENSAE\\C++\\Projet\\WinesClassification\\Data\\testing_dataset.csv", predict_var, predict_labels, nb_predict_samples,nb_var);
+	
+	nnmodel.predict(predict_var, predict_results); //Fill the predict_results matrix with the probability of being in a class for each sample
+
+	cv::Mat classification_results(nb_predict_samples,1,CV_32FC2); //Matrix of the results of the classification using MLP (without probabilities)
+
+	for (int i = 0; i < predict_results.rows; i++)
+	{
+		float predicted_class = predict_results.at<float>(0, 0);
+
+		for (int j = 0; j < predict_results.cols; j++)
+		{
+			if (predict_results.at<float>(0, j) > predicted_class)
+			{
+				predicted_class = predict_results.at<float>(0, j);
+			}
+			int sample_classification = predicted_class;
+			classification_results.at<float>(i,0) = sample_classification; //working??
+		}
+	}
+
+	//The matrix of the results for each sample is now filled
+	printf("Predicted quality of wines:\n");
+	for (int i = 0; i < classification_results.rows; i++)
+	{
+		printf("Sample %d: class %d", i, classification_results.at<float>(i));
+	}
+
+	//2- We have only one sample to classify
+
+	//Make a method that takes that sample and that calculate the predicted class using the MLP model: nnmodel.predict(sample,result)
+
+	/**************************
+	WORK TO DO:
+	- The basic code is done but needs the data
+	- Makes methods with the predict and 'extract the right predicted class' things
+	- Re-organize the code into classes & methods ? --> object = MLP?
+	- Method fot the very last point
+	- Export to Qt
+	***************************/
+
+	/**************************
+	WHAT IS DONE?
+	- Creation of the MLP model -->perhaps add some error controlling methods and a 'number of layers in the network' method
+	- Training and testing of the neural network
+	- Save/ load the file containing the model
+	- Predict the class of any external sample --> perhaps add a function 'recalcultate the model' if you don't want to use the .xml saved model
+	***************************/
 }
